92fa7bfbcc905f0e4cbcacc7f82593d46e2f5c74
manuelguirado <manu22.mgb@gmail.com>
2025-06-13 18:47:48 +0200

    add: implement chain of responsability

---
+++ chainofresponsability/component.ts
@@ -0,0 +1,73 @@
+
+interface componentWithContextualHelp {
+    showHelp: () => void;
+}
+abstract class Component implements componentWithContextualHelp {
+    private helpText: string;
+    protected container?: container;
+    protected constructor(helpText: string, container?: container) {
+        this.helpText = helpText;
+        this.container = container;
+    }
+    showHelp(){
+        if (this.helpText) {
+            console.info("Help text for this component:", this.helpText);
+        } else {
+            console.warn("No help text available for this component.");
+        }
+    }
+    
+  
+}
+abstract class container extends Component {
+    protected children: Component[] = [];
+    constructor(helpText: string, container?: container) {
+        super(helpText, container);
+    }
+    addChild(child: Component) {
+        this.children.push(child);
+      
+    }
+    showHelp() {
+        super.showHelp();
+        this.children.forEach(child => child.showHelp());
+    }
+}
+class Button extends Component {
+    constructor(helpText: string, container: container) {
+        super(helpText, container);
+    }
+}
+class dialog extends container {
+    constructor(helpText: string, container?: container) {
+        super(helpText, container);
+    }
+}
+class dialogButton extends Button {
+    constructor(helpText: string, container: container) {
+        super(helpText, container);
+    }
+}
+class application {
+    createUI() {
+        const mainContainer = new dialog("Main dialog help text" );
+        const button1 = new Button("Button 1 help text", mainContainer);
+        const button2 = new Button("Button 2 help text", mainContainer);
+        const subDialog = new dialog("Sub dialog help text", mainContainer);
+        const subButton = new dialogButton("Sub dialog button help text", subDialog);
+
+        mainContainer.addChild(button1);
+        mainContainer.addChild(button2);
+        mainContainer.addChild(subDialog);
+        subDialog.addChild(subButton);
+
+        return mainContainer;
+    }
+    showHelp() {
+        const ui = this.createUI();
+        ui.showHelp();
+    }
+}
+// Example usage
+const chainApp = new application();
+chainApp.showHelp();  
